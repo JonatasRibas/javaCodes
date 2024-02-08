8f2392c2da5fd98ed04c651296de7fac1871b401
Jônatas Ribas <91293805+JonatasRibas@users.noreply.github.com>
2024-02-08 09:36:33 -0300

    commnit

--- generationCodes/day9/lacosCondicionais/ex2/src/Main.java
+++ generationCodes/day9/lacosCondicionais/ex2/src/Main.java
@@ -2,14 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int n1 = 4, n2 = 3, n3 = 4;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        if(n1 < 5) {
+            System.out.println("O número 1 é menor do que 5.");
         }
+
+        if(n1 < n2) {
+            System.out.println("O número 1 é menor do que o número 2.");
+        }
+
+        if(n1 == n3)
+            System.out.println("O número 1 e o número 3 são iguais.");
     }
 }
