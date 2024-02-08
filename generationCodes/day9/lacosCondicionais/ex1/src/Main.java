e901c8b863aabe4760ca41b685707183fe85fa46
Jônatas Ribas <91293805+JonatasRibas@users.noreply.github.com>
2024-02-08 08:59:14 -0300

    commit

---
+++ generationCodes/day9/lacosCondicionais/ex1/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        boolean x = true;
+        boolean y = false;
+
+        if (x){
+            System.out.println("X é verdadeiro");
+        }
+        else if (y){
+            System.out.println("Y é verdadeiro");
+        } else {
+            System.out.println("Sla");
+        }
+
+    }
+}
